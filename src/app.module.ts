888df528860541c5452828ff3445e9c175199470
atiqbitstream <atiq.bitstream@gmail.com>
2024-10-08 12:59:16 +0500

    created a 'faker' module to populate database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { FakerModule } from './faker/faker.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, FakerModule],
   controllers: [AppController],
   providers: [AppService],
 })
