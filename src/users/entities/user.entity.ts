2f5356ce239079bef504dbdebfdeb3e95104cb04
atiqbitstream <atiq.bitstream@gmail.com>
2024-10-08 12:10:39 +0500

    created 'user' entity wiht 'organizationId'

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Column, Entity } from "typeorm";
+
+
+@Entity()
+export class User {
+  @Column({ type: 'varchar', length: 150 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 150 })
+  lastName: string;
+
+  @Column({ nullable: true, type: 'varchar', length: 150 })
+  email: string;
+
+  @Column({ type: 'enum', enum: ERole })
+  role: ERole;
+
+  
+  @Column()
+  password:string;
+
+  @Column({ nullable: true })
+  organizationId?: string;
+
+  
+}
